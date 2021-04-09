1c46a9cc44218cc7e53b6a2118d5d7221f8d9c46
Sandy <sandyddinh@gmail.com>
2021-04-09 11:57:07 -0400

    wrote the Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,9 +7,9 @@ export default class Contact extends Component {
       <>
         {/* <Header /> */}
           <div id="contact">
-            LinkedIn: sandydinh
-            Email: sandyddinh@gmail.com
-            Github: sandyddinh
+            LinkedIn: <a href="https://www.linkedin.com/in/sandy-dinh/">sandy-dinh</a>
+            Email: <a href="mailto:sandyddinh@gmail.com">sandyddinh@gmail.com</a>
+            Github: <a href="https://github.com/sandyddinh">sandyddinh</a>
           </div>
       </>
     );
