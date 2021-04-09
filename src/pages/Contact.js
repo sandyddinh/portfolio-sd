ef80bb1982220d1a3e68323b5e35131585bcd13e
Sandy <sandyddinh@gmail.com>
2021-04-09 17:56:39 -0400

    styled the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import linkedIn from '../img/linkedin.svg';
+import github from '../img/github.svg';
+import mail from '../img/mail.svg';
 
 export default function Contact () {
     return (
       <div className="ContactPage">
           <div id="contact">
-            LinkedIn: <a href="https://www.linkedin.com/in/sandy-dinh/">sandy-dinh</a>
-            Email: <a href="mailto:sandyddinh@gmail.com">sandyddinh@gmail.com</a>
-            Github: <a href="https://github.com/sandyddinh">sandyddinh</a>
+            <div className="top"></div>
+            <div className="social">
+              <img src={github} className="social-icon"/> <a href="https://github.com/sandyddinh">sandyddinh</a>
+            </div>
+            <div className="social">
+              <img src={linkedIn} className="social-icon"/> <a href="https://www.linkedin.com/in/sandy-dinh/">sandy-dinh</a>
+            </div>
+            <div className="social">
+              <img src={mail} className="social-icon"/> <a href="mailto:sandyddinh@gmail.com">sandyddinh@gmail.com</a>
+            </div>
           </div>
       </div>
     );
