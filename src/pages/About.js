96d62cffb2088a2d0f667d59d755aa182a916547
Sandy <sandyddinh@gmail.com>
2021-04-09 17:20:48 -0400

    added toggles to open and close project details

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,22 +7,24 @@ export default class About extends Component {
       <>
         <Header/>
         <div id="about">
-          <h1>I design and build websites.</h1>
-          
-          <h2>I'm a multi-faceted human with over 6+ years of experience in management consulting, financial services, and digital engineering</h2>
-          
-          <p>
-            During my time in management consulting, I served a wide range of roles including Project Management and Business Analysis before finding my way to Digital Engineering projects as a QA Engineer. I helped a large Brazilian bank launch their millenial digital-only banking app. 
-          </p>
+          <div className="about-body">
+            <h1>I design and build websites.</h1>
+            <br/>
+            <h2>I'm a multi-faceted human with over 6+ years of experience in management consulting, financial services, and digital engineering.</h2>
+            <br/>
+            <p>
+              During my time in management consulting, I served a wide range of roles including Project Management and Business Analysis before finding my way to Digital Engineering projects as a QA Engineer. I helped a large Brazilian bank launch their millenial digital-only banking app. 
+            </p>
+            <br/>
+            <p>
+              When the pandemic hit, I comitted to developing my software engineering skillsets by completing an immersive bootcamp at General Assembly while still working a full time job. This experience re-affirmed my passion of coding. I hope you check out my portfolio.
+            </p>
+            <br/>
+            <p>
+              My other passion is yoga. I have been practicing for over 5 years and obtained my Yoga Teacher Training 200hr cerfication in 2018. My practice has influenced my mindful approach to coding.
+            </p>
 
-          <p>
-            When the pandemic hit, I comitted to developing my software engineering skillsets by completing an immersive bootcamp at General Assembly while still working a full time job. This experience re-affirmed my passion of coding. I hope you check out my portfolio.
-          </p>
-
-          <p>
-            My other passion is yoga. I have been practicing for over 5 years and obtained my Yoga Teacher Training 200hr cerfication in 2018. My practice has influenced my mindful approach to coding.
-          </p>
-
+          </div>
         </div>
         </>
     );
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,107 @@
-import React, { Component } from "react";
-import Header from '../components/Header';
+import React, { Component, useState } from 'react';
+import cantaloupeClothing from '../img/cantaloupe-clothing.png';
+import cantaloupeItem from '../img/cantaloupe-item.png';
+import cantaloupeCheckout from '../img/cantaloupe-checkout.png';
+import journalMain from '../img/journal-main.png';
+import journalEntry from '../img/journal-entry.png';
+import journalAbout from '../img/journal-about.png';
+import rewindIntro from '../img/rewind-intro.png';
+import rewindBoard from '../img/rewind-board.png';
+import rewindHelp from '../img/rewind-help.png';
 
-export default class Porfolio extends Component {
-  render() {
+
+
+export default function Porfolio() {
+  const [openCantaloupe, setOpenCantaloupe] = useState(false);
+  const [openJournal, setOpenJournal] = useState(false);
+  const [openRewind, setOpenRewind] = useState(false);
+
+  const cantaloupeToggle = e => {
+    e.preventDefault();
+    setOpenCantaloupe(!openCantaloupe);
+  }
+
+  const journalToggle = e => {
+    e.preventDefault();
+    setOpenJournal(!openJournal);
+  }
+
+  const rewindToggle = e => {
+    e.preventDefault();
+    setOpenRewind(!openRewind);
+  }
+  
     return (
-      <>
+      <div className="PortfolioPage">
        {/* <Header/> */}
         <div id="portfolio">
-          Add 3 portfolio projects
+          <div className="cantaloupe-project">
+            <div className="cantaloupe-header">
+              <h2>Cantaloupe</h2>
+              <h3>An e-commerce store.</h3>
+              <button onClick={cantaloupeToggle}>{openCantaloupe ? "-" : "+"}</button>
+            </div>
+            <div className={openCantaloupe ? "cantaloupe-body" : "hide-details"}> 
+              <div className="project-preview">
+                <div className="project-image"><img src={cantaloupeClothing}/></div>
+                <div className="project-image"><img src={cantaloupeItem}/></div>
+                <div className="project-image"><img src={cantaloupeCheckout}/></div>
+              </div>
+              <p>Technologies:</p>
+              <ul>
+                <li>Mongo DB</li>
+                <li>Express</li>
+                <li>React</li>
+                <li>NodeJS</li>
+                <li>Stripe Payments</li>
+                <li>SCSS</li>
+              </ul>
+            </div>
+          </div>
+          <div className="journal-project">
+            <div className="journal-header">
+              <h2>My Daily</h2>
+              <h3>A daily journal.</h3>
+              <button onClick={journalToggle}>{openJournal ? "-" : "+"}</button>
+            </div>
+            <div className={openJournal ? "journal-body" : "hide-details"}>
+              <div className="project-preview">
+                <div className="project-image"><img src={journalMain}/></div>
+                <div className="project-image"><img src={journalEntry}/></div>
+                <div className="project-image"><img src={journalAbout}/></div>
+              </div>
+              <p>Technologies:</p>
+              <ul>
+                <li>Mongo DB</li>
+                <li>Express</li>
+                <li>React</li>
+                <li>NodeJS</li>
+                <li>External API</li>
+                <li>SCSS</li>
+              </ul>
+            </div> 
+          </div>
+          <div className="rewind-project">
+            <div className="rewind-header">
+              <h2>2020 In Rewind</h2>
+              <h3>A trivia game inspired by Family Feud.</h3>
+              <button onClick={rewindToggle}>{openRewind ? "-" : "+"}</button>
+            </div>
+            <div className={openRewind ? "rewind-body" : "hide-details"}>
+              <div className="project-preview">
+                <div className="project-image"><img src={rewindIntro}/></div>
+                <div className="project-image"><img src={rewindBoard}/></div>
+                <div className="project-image"><img src={rewindHelp}/></div>
+              </div>
+              <p>Technologies:</p>
+              <ul>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </>
+      </div>
     );
-  }
 }
