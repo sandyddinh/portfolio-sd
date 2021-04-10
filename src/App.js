b8d9d9c61ba8aa87934e28c90395207a4dfca78b
Sandy <sandyddinh@gmail.com>
2021-04-09 20:19:41 -0400

    styled the contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Resume from "./pages/Resume";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 
+
 class App extends Component {
   render() {
     return (
