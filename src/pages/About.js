fc688abe22aa44bcf60f9fbf497093124dec6dce
Sandy <sandyddinh@gmail.com>
2021-04-11 16:40:34 -0400

    wrote the Resume page

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ export default function About () {
           {/* <div className="about-body"> */}
             <h1>I design and build websites.</h1>
             <br/>
-            <h2>I'm a multi-faceted human with over 6+ years of experience in management consulting, financial services, and digital engineering.</h2>
+            <h2>I'm a multi-faceted human with over 6+ years of experience in management consulting and digital engineering.</h2>
             <br/>
             <p>
               During my time in management consulting, I served a wide range of roles including Project Management and Business Analysis before finding my way to Digital Engineering projects as a QA Engineer. I helped a large Brazilian bank launch their millenial digital-only banking app. 
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,7 +4,26 @@ export default function Resume () {
     return (
       <div className="ResumePage">
           <div id="resume">
-            This is the resume page.
+            <h2>Engineering</h2>
+              - Full Stack Web Developer
+              - Test Automation
+            
+            <h2>Technology</h2>
+              - JavaScript
+              - React
+              - HTML
+              - CSS
+              - SQL
+              - Mongo
+              - Postgres
+              - RESTful APIs
+            
+            <h2>Business</h2>
+              - Business Process Management / Optimization
+              - Business Analysis
+              - Requiremnts Gathering
+              - Stakeholder Management
+            
           </div>
       </div>
     );
