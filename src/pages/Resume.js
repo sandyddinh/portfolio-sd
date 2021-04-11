0fe49eb4bcc3d80af99eeba7160ef5f9f286b218
Sandy <sandyddinh@gmail.com>
2021-04-11 16:51:41 -0400

    added some box shadow to Portfolio page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,10 +5,12 @@ export default function Resume () {
       <div className="ResumePage">
           <div id="resume">
             <h2>Engineering</h2>
+            <hr/>
               - Full Stack Web Developer
               - Test Automation
             
             <h2>Technology</h2>
+            <hr/>
               - JavaScript
               - React
               - HTML
@@ -19,6 +21,7 @@ export default function Resume () {
               - RESTful APIs
             
             <h2>Business</h2>
+            <hr/>
               - Business Process Management / Optimization
               - Business Analysis
               - Requiremnts Gathering
