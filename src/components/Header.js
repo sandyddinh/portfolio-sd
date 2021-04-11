e5b67595650493751d6cc1efe9af5d55fc88f87a
Sandy <sandyddinh@gmail.com>
2021-04-11 16:21:01 -0400

    styled the nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,12 @@ export default class Header extends Component {
   render() {
     return (
         <header id="header" className="sticky">
-          <a href="#about">About Me</a>
-          <a href="#resume">Resume</a>
-          <a href="#portfolio">Portfolio</a>
-          <a href="#contact">Contact</a>
+          <div className="nav-links">
+            <a href="#about">About Me</a>
+            <a href="#resume">Resume</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contact</a>
+          </div>
         </header>
     );
   }
