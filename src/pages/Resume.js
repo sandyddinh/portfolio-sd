adee4df4bdaac5d1a5b6de010af5713fc0f0cacc
Sandy <sandyddinh@gmail.com>
2021-04-13 19:21:15 -0400

    styled the resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,29 +4,43 @@ export default function Resume () {
     return (
       <div className="ResumePage">
           <div id="resume">
-            <h2>Engineering</h2>
-            <hr/>
-              - Full Stack Web Developer
-              - Test Automation
-            
-            <h2>Technology</h2>
-            <hr/>
-              - JavaScript
-              - React
-              - HTML
-              - CSS
-              - SQL
-              - Mongo
-              - Postgres
-              - RESTful APIs
-            
-            <h2>Business</h2>
-            <hr/>
-              - Business Process Management / Optimization
-              - Business Analysis
-              - Requiremnts Gathering
-              - Stakeholder Management
-            
+            <div className="resume-category">
+              <h2>Engineering</h2>
+              <hr/>
+                <div className="skill-group">
+                  <div className="skill-tag">Full Stack Web Developer</div>
+                  <div className="skill-tag">Test Automation</div>
+                </div>
+            </div>
+
+            <div className="resume-category">
+              <h2>Technology</h2>
+              <hr/>
+              <div className="skill-group">
+                <div className="skill-tag">JavaScript</div>
+                <div className="skill-tag">React</div>
+                <div className="skill-tag">HTML</div>
+                <div className="skill-tag">CSS</div>
+                <div className="skill-tag">SQL</div>
+                <div className="skill-tag">Mongo</div>
+                <div className="skill-tag">Ruby on Rails</div>
+                <div className="skill-tag">Postgres</div>
+                <div className="skill-tag">RESTful APIs</div>
+                <div className="skill-tag">JIRA</div>
+              </div>
+            </div>
+
+            <div className="resume-category">
+              <h2>Business</h2>
+              <hr/>
+              <div className="skill-group">
+                <div className="skill-tag">Business Process Management / Optimization</div>
+                <div className="skill-tag">Business Analysis</div>
+                <div className="skill-tag">Requiremnts Gathering</div>
+                <div className="skill-tag">Stakeholder Management</div>
+              </div>
+            </div>
+
           </div>
       </div>
     );
